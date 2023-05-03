89ed1bf43270344d1b7194397e4067247d1e4cfc
ItsMeAlec <alecsouthward@gmail.com>
2023-05-03 10:47:20 +0200

    deleted:    ../blog/models (Alec Southward's conflicted copy 2023-04-21).py 	modified:   ../blog/models.py 	modified:   ../blog/views.py 	modified:   ../user_auth/views.py 	../__pycache__/ 	../blog/__pycache__/ 	../blog/migrations/__pycache__/ 	../db.sqlite3 	./ 	../mySite/__pycache__/ 	../user_auth/__pycache__/ 	../user_auth/migrations/__pycache__/

--- mySite/user_auth/views.py
+++ mySite/user_auth/views.py
@@ -6,9 +6,17 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 def user_login(request):
+    """This renders the  login.html page."""
     return render(request, 'authentication/login.html')
 
 def authenticate_user(request):
+    """This will either render the login page,
+        or will send the user to the index page.
+
+        It will decide which after checking if the user
+        is logged in and a valid user.
+    """
+
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
@@ -22,7 +30,11 @@ def authenticate_user(request):
             reverse('index')
         )
     
-def show_user(request):
+def show_user(request):    
+    """This is a testing page used to see the
+        users name and encrypted password.
+    """
+
     print(request.user.username)
     return render(request, 'authentication/user.html', {
         "username": request.user.username,
@@ -30,9 +42,22 @@ def show_user(request):
     })
 
 def create_user(request):
+    """This will render the create_user.html (the user register) page."""
+
     return render(request, 'authentication/create_user.html')
 
 def reg_user(request):
+    """This will either render the create_user.html (the user register) page,
+        or will send the user to the index page.
+
+        It will create a User object based on what the user
+        input on the page.
+
+        After checking if the User object is valid it will send the user
+        back to the index page, otherwise it will send the user back
+        to the register page.
+    """
+
     username = request.POST['username']
     password = request.POST['password']
 
